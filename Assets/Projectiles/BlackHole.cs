559785cd8e0950e425b60afc83dcab99f30105bd
Hich2000 <33690542+Hich2000@users.noreply.github.com>
2022-07-21 21:25:49 +0200

    fixed star spawning radius and other bug
    made the radius a fixed length
    fixed issue where if black hole projectile timed naturally it could not be spawned anymore.

--- Assets/Projectiles/BlackHole.cs
+++ Assets/Projectiles/BlackHole.cs
@@ -11,8 +11,11 @@ namespace TheSauce.Assets.Projectiles
     internal class BlackHole : ModProjectile
     {
 
+		public float radius = 800;
+
 		public static int maxInstances = 1;
 		public static BlackHole[] blackHoleInstances = new BlackHole[maxInstances];
+		public Random spawnRandomizer = new Random();
 
 		public override void SetDefaults()
 		{
@@ -21,17 +24,39 @@ namespace TheSauce.Assets.Projectiles
 			Projectile.friendly = true;
 			Projectile.penetrate = 1000;
 			Projectile.timeLeft = 600;
+			Projectile.ai[0] = 0;
 		}
 
-        public override void OnSpawn(IEntitySource source)
+        public override void AI()
+        {
+			Projectile.ai[0] = (Projectile.ai[0]+1)%30;
+
+			if (Projectile.ai[0] == 0)
+            {
+				float angle = (float)(2.0 * Math.PI * spawnRandomizer.NextDouble());
+
+				Vector2 spawnPosition = Projectile.Center;
+
+				spawnPosition.X = (float)(spawnPosition.X + (Math.Cos(angle) * radius));
+				spawnPosition.Y = (float)(spawnPosition.Y + (Math.Sin(angle) * radius));
+
+				Projectile.NewProjectile(Projectile.InheritSource(Projectile), spawnPosition, Vector2.Zero, ModContent.ProjectileType<BlackHoleStar>(), 5, 0, Projectile.owner);
+			}
+        }
+
+		public override void Kill(int timeLeft = 0)
+		{
+			base.Kill(0);
+			blackHoleInstances[0] = null;
+		}
+
+		public override void OnSpawn(IEntitySource source)
         {
             base.OnSpawn(source);
-			//todo check for instance of BlackHole and kill it.
 			if (blackHoleInstances[0] != null)
             {
 				blackHoleInstances[0].Projectile.Kill();
 				blackHoleInstances[0] = this;
-				//Projectile.Kill();
 			} else
             {
                 blackHoleInstances[0] = this;
---
+++ Assets/Projectiles/BlackHoleStar.cs
@@ -0,0 +1,24 @@
+﻿using TheSauce.Assets.Dusts;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.DataStructures;
+using System;
+
+namespace TheSauce.Assets.Projectiles
+{
+	internal class BlackHoleStar : ModProjectile
+	{
+
+		public override void SetDefaults()
+		{
+			Projectile.width = 22;
+			Projectile.height = 24;
+			Projectile.friendly = true;
+			Projectile.penetrate = 5;
+			Projectile.timeLeft = 600;
+		}
+
+	}
+}
